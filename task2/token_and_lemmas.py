805335cc0efa20fca1038b3df8143cc1f4d4a814
rustemd02 <rustemd02@mail.ru>
2024-03-04 10:58:55 +0300

    task3

--- task2/token_and_lemmas.py
+++ task2/token_and_lemmas.py
@@ -6,7 +6,6 @@ from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 import pymorphy2
 
-
 def extract_text_from_html(file_path):
     with open(file_path, 'r', encoding='utf-8') as file:
         html_content = file.read()
@@ -14,20 +13,18 @@ def extract_text_from_html(file_path):
     text = soup.get_text()
     return text
 
-
-def clean_and_tokenize(text):
-    text = re.sub(r'\W+', ' ', text)
+def clean_and_tokenize(text, stop_words):
+    text = re.sub(r'[^\w\s]', ' ', text)
+    text = re.sub(r'\d+', ' ', text)
     tokens = word_tokenize(text.lower())
-    tokens = [token for token in tokens if token.isalpha() and token not in stop_words]
+    tokens = [token for token in tokens if re.fullmatch(r'[а-яё]+', token) and token not in stop_words]
     return tokens
 
-
-def lemmatize(tokens):
+def lemmatize(tokens, morph):
     lemmas = [morph.parse(token)[0].normal_form for token in tokens]
     return lemmas
 
-
-def process_files(directory):
+def process_files(directory, morph, stop_words):
     all_tokens = set()
     lemmas_dict = {}
 
@@ -35,9 +32,9 @@ def process_files(directory):
         if filename.endswith('.html'):
             file_path = os.path.join(directory, filename)
             text = extract_text_from_html(file_path)
-            tokens = clean_and_tokenize(text)
+            tokens = clean_and_tokenize(text, stop_words)
             all_tokens.update(tokens)
-            lemmas = lemmatize(tokens)
+            lemmas = lemmatize(tokens, morph)
             for lemma, original in zip(lemmas, tokens):
                 if lemma in lemmas_dict:
                     lemmas_dict[lemma].add(original)
@@ -52,13 +49,10 @@ def process_files(directory):
         for lemma, original_tokens in sorted(lemmas_dict.items()):
             f.write(f"{lemma} {' '.join(sorted(original_tokens))}\n")
 
-
 if __name__ == '__main__':
     nltk.download('punkt')
     nltk.download('stopwords')
-
-    directory_path = '/Users/unterlantas/PycharmProjects/infosearch/task1/downloaded_pages'
-    process_files(directory_path)
-
     morph = pymorphy2.MorphAnalyzer()
     stop_words = set(stopwords.words('russian'))
+    directory_path = '/Users/unterlantas/PycharmProjects/infosearch/task1/downloaded_pages'
+    process_files(directory_path, morph, stop_words)
